0db3c418d0f4d527c7675ece6c19a9f9ee2639e3
GYANARANJAN PANDA <119920241+gyanaranjanpanda16@users.noreply.github.com>
2024-02-08 08:12:10 +0530

    java program added :--

--- src/main/java/com/gyanaranjan/ArrayProgram01/SwapArrayElement.java
+++ src/main/java/com/gyanaranjan/ArrayProgram01/SwapArrayElement.java
@@ -1,7 +1,17 @@
 package com.gyanaranjan.ArrayProgram01;
-
+// left - rotation Array :-->
  class SwapArrayElement {
   public static void main(String[] args) {
+      int [] arr1 = { 1,2,3,4,5,6,7,8};
+      int [] swaparr = new int[arr1.length];
+      int number = 5 ;
+      for(int i=0; i< arr1.length;i++){
+          int rotation = (i+number)% arr1.length;
+          swaparr[rotation]=arr1[i];
+      }
+      for(int a:swaparr){
+          System.out.print(a + " ");
+      }
 
      }
 }
